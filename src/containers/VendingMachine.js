d2d8b3f2ee505d45514f7092db7d1d134b95ef7e
Edward Sparks <edd_sparks@hotmail.com>
2019-01-09 14:00:21 +0000

    changed the API url back after testing

--- src/containers/VendingMachine.js
+++ src/containers/VendingMachine.js
@@ -15,10 +15,10 @@ export default class VendingMachine extends Component {
   }
 
   componentDidMount() {
-    return fetch(`https://vm-cleo-frontend.herokuapp.com//api/v1/machines/1/snacks/`)
+    return fetch(`https://vm-cleo-backend.herokuapp.com//api/v1/machines/1/snacks/`)
       .then(resp => resp.json())
       .then(data => this.setState({snacks: data.data}))
-      .then(fetch(`https://vm-cleo-frontend.herokuapp.com//api/v1/machines/1/money`)
+      .then(fetch(`https://vm-cleo-backend.herokuapp.com//api/v1/machines/1/money`)
         .then(resp => resp.json())
         .then(data => this.setState({denominations: data.data})))
   }
@@ -50,7 +50,7 @@ export default class VendingMachine extends Component {
 
   buySnacks = () => {
     let {selectedSnacks, insertedCoins, paid, total, denominations} = this.state
-    fetch(`https://vm-cleo-frontend.herokuapp.com//api/v1/machines/1`, {
+    fetch(`https://vm-cleo-backend.herokuapp.com//api/v1/machines/1`, {
       method: 'PATCH',
       headers: {
         Accept: 'application/json',
